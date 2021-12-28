2396bd3b7a02d64e6a9e481bfaa940a19a510ea4
P Darwin <paulosperneta@gmail.com>
2021-12-28 17:05:59 +0000

    act

---
+++ login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login Wikimedia</title>
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+.button2 {
+  background-color: white; 
+  color: black; 
+  border: 3px solid #008CBA;
+}
+div {text-align: center;}
+</style>
+</head>
+<body>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include './bin/api.php';
+loginAPI($_GET["login"], $_GET["pass"]);
+echo "<BR><BR> O seu utilizador é:" . userinfoAPI ()["query"]["userinfo"]["name"];
+
+echo "<div>
+<form action=\"arq.php\" method=\"POST\">
+	<button class=\"button button2\" type=\"submit\" formaction=\"arq.php\">Arquipelagos</button>
+</form>
+</div>"
+
+
+
+
+
+?>
+</body>
+</html>
